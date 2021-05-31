8d3b6b2ea497cd982a23c52a1bcd1af5d79b1a7a
SHRAYAS RAGHUNATHAN <72727518+SHRAYAS-RAGHU@users.noreply.github.com>
2021-05-31 22:17:20 +0530

    Update vcgencmd commmands.py

--- TEMP_&_CPU-UTILISTAITON_MEASUREMENT/vcgencmd commmands.py
+++ TEMP_&_CPU-UTILISTAITON_MEASUREMENT/vcgencmd commmands.py
@@ -1,14 +1,14 @@
-from vcgencmd import Vcgencmd # MODULE TO MEASURE CPU DATA
-vcgm = Vcgencmd() # OBJECT OF VCGENCMD CLASS
+from vcgencmd import Vcgencmd                                           # MODULE TO MEASURE CPU DATA
+vcgm = Vcgencmd()                                                       # OBJECT OF VCGENCMD CLASS
 
 # FOR MEASURING THE STATS THIS INSTANCE IS USED TO ACCESS THE MEMBERS OF THE CLASS. 
 
 state = {'cpu_temp': [], 'cpu_volt': [], 'cpu_freq': [], 'arm_freq': []} # DICTIONARY TO STORE CPU STATS
 for i in range(5):     
-    cpu_temp = vcgm.measure_temp()                # MEASURING THE TEMPARATURE OF CORE
-    cpu_volt = vcgm.measure_volts('core')         # MEASURING THE VOLTAGE OF CORE
-    cpu_freq = vcgm.measure_clock('core')/1000000 # MEASURING THE FREQUENCY OF CORE
-    arm_freq = vcgm.measure_clock('arm')/1000000  # MEASURING THE FREQUENCY OF ARM
+    cpu_temp = vcgm.measure_temp()                                       # MEASURING THE TEMPARATURE OF CORE
+    cpu_volt = vcgm.measure_volts('core')                                # MEASURING THE VOLTAGE OF CORE
+    cpu_freq = vcgm.measure_clock('core')/1000000                        # MEASURING THE FREQUENCY OF CORE
+    arm_freq = vcgm.measure_clock('arm')/1000000                         # MEASURING THE FREQUENCY OF ARM
     
     # VALUES MEASURED ARE STORED IN THE DICTIONARY
     

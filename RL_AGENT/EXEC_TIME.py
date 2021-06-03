a824dcaff82c384698750e5e92b9441562e18924
SHRAYAS-RAGHU <krshrayas@gmail.com>
2021-06-03 12:05:12 +0530

    change

--- RL_AGENT/EXEC_TIME.py
+++ RL_AGENT/EXEC_TIME.py
@@ -1,10 +1,13 @@
 import subprocess
 import re  
 
-a = subprocess.Popen(['time','-p','python3','WORK.py'],
-    cwd='/home/pi/Desktop/PROJECT/RL_AGENT',
-    stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=False)
+for i in range(1000):
+    a = subprocess.Popen(['time','-f', '%P','python3','WORK.py'],
+        cwd='/home/pi/Desktop/PROJECT/RL_AGENT',stdout=subprocess.PIPE
+        ,stderr=subprocess.PIPE,shell=False)
+    print(str(a.communicate()[1]).lstrip('b\'').rstrip('\\n\''))
 
+"""
 a = str(a.communicate()[1])
 a = a.split('\\n')[:-1]
 b = []
@@ -12,3 +15,4 @@ for i in a:
     x = re.search(r'[\d.]+', i)
     b.append(float(x.group()))
 print(b)
+"""
